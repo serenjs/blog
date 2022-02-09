dda97143fde3f7c54f9a6bdd6b64de26f83182cb
serenjs <david.vidovic.web@gmail.com>
2022-02-09 22:12:27 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to my blog" />
         <p className="description">
           test! commit
         </p>
