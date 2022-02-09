3c7cef8c3174ffda438a22db880cec9427c417da
serenjs <david.vidovic.web@gmail.com>
2022-02-09 23:08:44 +0100

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,7 @@ import styles from './Header.module.css'
 
 export default function Header({ title }, { logo }) {
   return (
-  <div className={styles.footer}>
+  <div className={styles.header}>
     <div className='logo'>{logo}</div>
   <h1 className="title">{title}</h1>
   </div>
