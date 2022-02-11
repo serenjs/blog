cf04ae840a8c029b673586ead7eb3e982596448f
David Vidovic <david.vidovic.web@gmail.com>
2022-02-11 12:23:07 +0100

    Constructing header

--- components/Header.js
+++ components/Header.js
@@ -3,9 +3,11 @@ import styles from './Header.module.css'
 export default function Header({ title }) {
   return (
   <div className={styles.header}>
-  <h3 className='brand'>{title}</h3>
-  <div className='theme-switcher'></div>
-  <div className='menu'></div>
+  <h3 className={styles.brand}>{title}</h3>
+  <div className={styles.headerMenu}>
+    <div className={styles.themeSwitcher}></div>
+    <div className={styles.menu}></div>
+  </div>
   </div>
   );
 }
